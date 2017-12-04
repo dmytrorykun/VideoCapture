0bc5e6535041c065646f2f20e09a39e38878ca84
Dmitry Rykun <d.rykun@keepsolid.com>
2017-12-04 20:51:38 +0200

    Initial Commit

---
+++ VideoCapture/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  VideoCapture
+//
+//  Created by user on 12/4/17.
+//  Copyright © 2017 peoplecanfly. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ VideoCapture/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  VideoCapture
+//
+//  Created by user on 12/4/17.
+//  Copyright © 2017 peoplecanfly. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
